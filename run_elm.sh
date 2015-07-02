424aeab1e5ef52935ad75b36c558bacda4dcfa58
Mark J. Reed <mark.reed@turner.com>
2015-07-02 17:31:34 -0400

    use bundler to run elm test

--- run_elm.sh
+++ run_elm.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-exec ruby -rselenium-webdriver -e 'Selenium::WebDriver.for(:firefox).tap{|d| d.get("file://'"$PWD/build/${1%.*}.html"'"); puts d.find_elements(tag_name:"div")[1].text}.quit'
+bundle install --path vendor/bundle >& /dev/null
+bundle exec ruby -e 'require "selenium-webdriver"; Selenium::WebDriver.for(:firefox).tap{|d| d.get("file://'"$PWD/build/${1%.*}.html"'"); puts d.find_elements(tag_name:"div")[1].text}.quit'
