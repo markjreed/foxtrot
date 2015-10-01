0cc514273cfb30ecd61b6221a43cf0fc549c5447
Mark J. Reed <markjreed@gmail.com>
2015-10-01 14:26:32 -0400

    selenium doesn't work with latest firefox

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source "https://rubygems.org"
+gem "pry-byebug"
 gem "selenium"
 gem "selenium-webdriver"
+gem "chromedriver-helper"
--- run_elm.sh
+++ run_elm.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 bundle install --path vendor/bundle >& /dev/null
-bundle exec ruby -e 'require "selenium-webdriver"; Selenium::WebDriver.for(:firefox).tap{|d| d.get("file://'"$PWD/build/${1%.*}.html"'"); puts d.find_elements(tag_name:"div")[1].text}.quit'
+bundle exec ruby -e 'require "selenium-webdriver";
+d=Selenium::WebDriver.for(:chrome);
+d.get("file://'"$PWD/build/${1%.*}.html"'");
+puts d.find_elements(tag_name:"div")[1].text;
+d.quit'
