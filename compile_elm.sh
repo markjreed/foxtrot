0457d516506fa77f63fa518f014c7bc3797e4abf
Mark J. Reed <mark.reed@turner.com>
2016-06-29 16:53:15 -0400

    updated for newer elm

--- compile_elm.sh
+++ compile_elm.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-exec elm "$@"
+elm package install elm-lang/core -y
+elm package install elm-lang/html -y
+exec elm make "$@" --output=punishment_elm.html
--- run_elm.sh
+++ run_elm.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-bundle install --path vendor/bundle >& /dev/null
+(bundle install --path vendor/bundle >& /dev/null
+bundle exec selenium install) >/dev/null 2>&1
 bundle exec ruby -e 'require "selenium-webdriver";
 d=Selenium::WebDriver.for(:chrome);
-d.get("file://'"$PWD/build/${1%.*}.html"'");
-puts d.find_elements(tag_name:"div")[1].text;
+d.get("file://'"$PWD/${1%.*}_elm.html"'");
+puts d.find_elements(tag_name:"body")[0].text;
 d.quit'
