9c0ca896b24023609a067a8330f30b2eb65811df
Mark J. Reed <mark.reed@turner.com>
2016-08-16 15:11:49 -0400

    add explicit paths to algol header and lib

--- compile_a60.sh
+++ compile_a60.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 base="${1%.a60}"
 marst -o "${base}_a60.c" "$1"
-gcc -o "$base" "${base}_a60.c" -lalgol -lm 
+gcc -o "$base" -I/opt/local/include -L/opt/local/lib "${base}_a60.c" -lalgol -lm 
