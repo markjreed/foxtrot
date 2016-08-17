572025fc5bcd4fe9df7a42a9efc2abfcb2bd8d02
Mark J. Reed <mark.reed@turner.com>
2016-08-17 18:44:39 -0400

    test improvements

--- compile_m3.sh
+++ compile_m3.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-dir="${1%.*}"
+dir="${1%.*}_dir"
 mkdir "$dir" && cp "$1" "$dir"/Main.m3 && cd "$dir" && cm3 -build 
--- run_elm.sh
+++ run_elm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-(bundle install --path vendor/bundle >& /dev/null
-bundle exec selenium install) >/dev/null 2>&1
-bundle exec ruby -e 'require "selenium-webdriver";
+#(bundle install --path vendor/bundle >& /dev/null
+#bundle exec selenium install) >/dev/null 2>&1
+ruby -e 'require "selenium-webdriver";
 d=Selenium::WebDriver.for(:chrome);
 d.get("file://'"$PWD/${1%.*}_elm.html"'");
 puts d.find_elements(tag_name:"body")[0].text;
