cafdcb17eff781cc917f5ccbf0966f10f39bf884
Mark J. Reed <markjreed@gmail.com>
2016-08-18 21:11:32 -0400

    use new open-source version

--- run_ps1.sh
+++ run_ps1.sh
@@ -1,4 +1,2 @@
-#!/usr/bin/env python
-import winrm
-import sys
-print winrm.Session('192.168.99.100', auth=('IEUser','Passw0rd!')).run_ps(open(sys.argv[1]).read()).std_out,
+#!/usr/bin/env bash
+powershell -File "$@"
