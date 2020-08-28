20fa293a979ee441f5510477bd8bcbcc1a38a4f1
Mark J. Reed <markjreed@gmail.com>
2020-08-28 00:28:22 -0400

    synch

--- compile_elm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if type -p elm >/dev/null 2>&1; then
-  elm init <<<y >&/dev/null
-  exec elm make "$@" --output=punishment_elm.html
-else
-  exit 2
-fi
--- run_elm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ruby -e 'require "selenium-webdriver";
-d=Selenium::WebDriver.for(:chrome);
-d.get("file://'"$PWD/${1%.*}_elm.html"'");
-puts d.find_elements(tag_name:"body")[0].text;
-d.quit'
