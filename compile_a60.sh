423125a131b5e02af575d61c0fa0b732c3fe4ddd
Mark J. Reed <mark.reed@warnermedia.com>
2021-07-10 02:57:03 -0400

    work against brew-style marst install

--- compile_a60.sh
+++ compile_a60.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 base="${1%.a60}"
 marst -o "${base}_a60.c" "$1"
-gcc -o "$base" -I/opt/local/include -L/opt/local/lib "${base}_a60.c" -lalgol -lm 
+gcc -o "$base" -I/usr/local/opt/marst/include -L/usr/local/opt/marst/lib "${base}_a60.c" -lalgol -lm 
