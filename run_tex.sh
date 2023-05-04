fa715c419a79fe15a03ca7fae4fea1527a0eb5f3
Mark J. Reed <markjreed@gmail.com>
2023-05-04 16:42:51 -0400

    feat: add LaTeX

---
+++ run_tex.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+xelatex "$1" >/dev/null
+open -a Preview "${1%.tex}.pdf"
+sleep 2
+osascript >/dev/null 2>&1 <<'END_SCRIPT'
+  tell application "Preview"
+	activate
+  end tell
+
+  tell application "System Events" to tell process "Preview"
+	tell menu 1 of menu bar item "Edit" of menu bar 1
+      click menu item "Select All"
+	  click menu item "Copy"
+	end tell
+    click menu item "Close Window" of menu 1 of menu bar item "File" of menu bar 1
+  end tell
+END_SCRIPT
+sleep 2
+pbpaste | tr $'\r' $'\n' | sed $'s/\./.\\\n/g' | sed -e 's/^ *//' | grep .
