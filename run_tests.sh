aa23a2e20b2581572f39df9cb710f0fc325dccb4
Mark J. Reed <markjreed@gmail.com>
2024-05-20 13:11:10 -0400

    fix: run tests in isolated subdirectories

--- run_tests.sh
+++ run_tests.sh
@@ -43,101 +43,104 @@ for lang in "${languages[@]}"; do
   if [ ! -r "$f" ]; then
     result='[33mSKIP[0m'
   else
+    mkdir "$ext"
+    mv "$f" "$ext"
+    (cd "$ext"
     result=
     compile=()
     run=()
     in=
-    if [ -x "compile_$ext.sh" ]; then
-      compile=("./compile_$ext.sh" "$f" )
+    if [ -x "../compile_$ext.sh" ]; then
+      compile=("../compile_$ext.sh" "$f" )
     fi
     if [ -x "run_$ext.sh" ]; then
-      run=("./run_$ext.sh" "$f" )
+      run=("../run_$ext.sh" "$f" )
     else
       case "$ext" in
-      11l) run=(python -m11l "$f");;
-      a68) run=(a68g "$f");;
-      adb) compile=(gnat make "$f"); run=("./${f%.$ext}");;
-      awk) run=(awk -f ./"$f");;
-      b) if ! type -p ybc >&/dev/null; then result='[33mSKIP[0m'; else compile=(bash -c "ybc -c "$PWD/$f"; clang -m32 $f.o /usr/local/opt/ybc/b-lib.o"); run=(./a.out); fi;;
-      bas) run=(cbmbasic "$f");;
-      bat) run=(4bat "$f");;
-      batsh) eval "$(opam env)";if ! type -p batsh >/dev/null; then result='[33mSKIP[0m'; else  compile=(bash -c "batsh bash '$f' >batsh.out"); run=(bash ./batsh.out); fi;;
-      bef) run=(bef -q "$f");;
-      bf) run=(bf); in="$f";;
-      c) compile=(gcc "$f" ); run=(./a.out);;
-      lisp) run=(clisp "$f");;
-      clj) run=(lein exec "$f");;
-      cob) compile=(cobc -x "$f"); run=("./${f%.$ext}");;
-      cpp) compile=(g++ "$f" ); run=(./a.out);;
-      cs) compile=(mcs "$f"); run=(mono "${f%.*}.exe");;
-      d) compile=(dmd "$f" ); run=("./${f%.$ext}");;
-      dc) run=(dc -f "$f");;
-      dylan) run=(rundylan "$f");;
-      elisp) run=(emacs --script "$f");;
-      elm) result='SKIP';;
-      erl) run=(escript "$f");;
-      exs) run=(elixir "$f");;
-      f) compile=(gfortran -std=legacy "$f" ); export GFORTRAN_UNBUFFERED_ALL=1; run=(./a.out);;
-      "><>") run=(python ~/bin/fish.py "$f");;
-      fr) compile=(fregec "$f" ); run=(frege Punishment);;
-      4th) run=(gforth "$f");;
-      go) run=(go run "$f");;
-      hs) . "$HOME/.ghcup/env"; run=(runhaskell "$f");;
-      hx) run=(haxe --main Punishment --interp);;
-      i) compile=(ick -b "$f"); run=("./${f%.$ext}");;
-      ico)  run=(icon "$f");;
-      idr) compile=(idris -o a.out "$f"); run=(./a.out);;
-      inf) compile=(inform "$f"); run=(dfrotz -mp "${f%.$ext}.z5");;
-      j) run=(jconsole "$f");;
-      java) compile=(javac "$f"); run=(java Punishment);;
-      js) run=(node "$f");;
-      kt) compile=(kotlinc "$f"); run=(kotlin PunishmentKt);;
-      lasso) run=(lasso "$f");;
-      lol) run=(lci "$f");;
-      lsl) result='[33mSKIP[0m';;
-      m) compile=(clang -framework Foundation "$f"); run=(./a.out);;
-      mad) compile=(mad "$f"); run=(./"${f%.*}");;
-      mod) compile=(gm2 -fno-pth -flibs=log,pim "$f"); run=(./a.out);;
-      m3) compile=(cm3); run=(bash -c "./*/prog"); rm -f punishment.*;;
-      ml) run=(ocaml  "$f");;
-      nim) run=(nim --verbosity:0 --run compile "$f");;
-      occ) compile=(kroc "$f" -lcourse); run=(./punishment);;
-      p) compile=(fpc "$f" ); run=("./${f%.$ext}");;
-      p4) run=(perl4 "$f");;
-      p5) run=(perl "$f");;
-      p5i) run=(perl5i "$f");;
-      raku) run=(raku "$f");;
-      pl) run=(swipl -q -g main -l "$f");;
-      ps1) run=(pwsh "$f");;
-      py2) run=(python2 "$f");;
-      py3) run=(python3 "$f");;
-      q) run=(qore "$f");;
-      r)  run=(r -q --slave -f "$f");;
-      rb) run=(ruby "$f");;
-      reb) run=(rebol -q "$f");;
-      rkt) run=(racket "$f");;
-      rs) compile=(rustc "$f"); run=("./${f%.$ext}");;
-      sci) run=(scilab-cli -nb -f "$f" -quit);;
-      scm) run=(scheme --quiet --load "$f" --eval '(exit)');;
-      semi) run=(semicolon "$f");;
-      sf) run=(sidef "$f");;
-      sim) compile=(cim "$f"); run=("./${f%.$ext}");;
-      sl) run=(slsh "$f");;
-      sml) compile=(mlton "$f"); run=("./${f%.$ext}");;
-      sno) run=(snobol4 "$f");;
-      spl) run=(splrun "$f");;
-      sq) run=(subleq "$f");;
-      st) run=(gst "$f");;
-      tcl) run=(tclsh "$f");;
-      ts) compile=(tsc "$f"); run=(node "${f%.$ext}.js");;
-      unl) run=(unlambda "$f");;
-      vala) compile=(valac "$f"); run=("./${f%.$ext}");;
-      vq) run=(varaq -s "$f");;
-      wren) run=(wren_cli "$f");;
-      yorick) run=(yorick -batch "$f");;
-      zig) run=(zig run "$f");;
-      🍇) compile=(emojicodec "$f"); run=("./${f%.$ext}");;
-      *) if type -t "$ext"  >/dev/null; then
+      (11l) run=(python -m11l "$f");;
+      (a68) run=(a68g "$f");;
+      (adb) compile=(gnat make "$f"); run=("./${f%.$ext}");;
+      (awk) run=(awk -f ./"$f");;
+      (b) if ! type -p ybc >&/dev/null; then result='[33mSKIP[0m'; else compile=(bash -c "ybc -c "$PWD/$f"; clang -m32 $f.o /usr/local/opt/ybc/b-lib.o"); run=(./a.out); fi;;
+      (bas) run=(cbmbasic "$f");;
+      (bat) run=(4bat "$f");;
+      (batsh) eval "$(opam env)";if ! type -p batsh >/dev/null; then result='[33mSKIP[0m'; else  compile=(bash -c "batsh bash '$f' >batsh.out"); run=(bash ./batsh.out); fi;;
+      (bef) run=(bef -q "$f");;
+      (bf) run=(bf); in="$f";;
+      (c) compile=(gcc "$f" ); run=(./a.out);;
+      (lisp) run=(clisp "$f");;
+      (clj) run=(lein exec "$f");;
+      (cob) compile=(cobc -x "$f"); run=("./${f%.$ext}");;
+      (cpp) compile=(g++ "$f" ); run=(./a.out);;
+      (cs) compile=(mcs "$f"); run=(mono "${f%.*}.exe");;
+      (d) compile=(dmd "$f" ); run=("./${f%.$ext}");;
+      (dc) run=(dc -f "$f");;
+      (dylan) run=(rundylan "$f");;
+      (elisp) run=(emacs --script "$f");;
+      (elm) result='SKIP';;
+      (erl) run=(escript "$f");;
+      (exs) run=(elixir "$f");;
+      (f) compile=(gfortran -std=legacy "$f" ); export GFORTRAN_UNBUFFERED_ALL=1; run=(./a.out);;
+      ("><>") run=(python ~/bin/fish.py "$f");;
+      (fr) compile=(fregec "$f" ); run=(frege Punishment);;
+      (4th) run=(gforth "$f");;
+      (go) run=(go run "$f");;
+      (hs) . "$HOME/.ghcup/env"; run=(runhaskell "$f");;
+      (hx) run=(haxe --main Punishment --interp);;
+      (i) compile=(ick -b "$f"); run=("./${f%.$ext}");;
+      (ico)  run=(icon "$f");;
+      (idr) compile=(idris -o a.out "$f"); run=(./a.out);;
+      (inf) compile=(inform "$f"); run=(dfrotz -mp "${f%.$ext}.z5");;
+      (j) run=(jconsole "$f");;
+      (java) compile=(javac "$f"); run=(java Punishment);;
+      (js) run=(node "$f");;
+      (kt) compile=(kotlinc "$f"); run=(kotlin PunishmentKt);;
+      (lasso) run=(lasso "$f");;
+      (lol) run=(lci "$f");;
+      (lsl) result='[33mSKIP[0m';;
+      (m) compile=(clang -framework Foundation "$f"); run=(./a.out);;
+      (mad) compile=(mad "$f"); run=(./"${f%.*}");;
+      (mod) compile=(gm2 -fno-pth -flibs=log,pim "$f"); run=(./a.out);;
+      (m3) compile=(cm3); run=(bash -c "./*/prog"); rm -f punishment.*;;
+      (ml) run=(ocaml  "$f");;
+      (nim) run=(nim --verbosity:0 --run compile "$f");;
+      (occ) compile=(kroc "$f" -lcourse); run=(./punishment);;
+      (p) compile=(fpc "$f" ); run=("./${f%.$ext}");;
+      (p4) run=(perl4 "$f");;
+      (p5) run=(perl "$f");;
+      (p5i) run=(perl5i "$f");;
+      (raku) run=(raku "$f");;
+      (pl) run=(swipl -q -g main -l "$f");;
+      (ps1) run=(pwsh "$f");;
+      (py2) run=(python2 "$f");;
+      (py3) run=(python3 "$f");;
+      (q) run=(qore "$f");;
+      (r)  run=(r -q --slave -f "$f");;
+      (rb) run=(ruby "$f");;
+      (reb) run=(rebol -q "$f");;
+      (rkt) run=(racket "$f");;
+      (rs) compile=(rustc "$f"); run=("./${f%.$ext}");;
+      (sci) run=(scilab-cli -nb -f "$f" -quit);;
+      (scm) run=(scheme --quiet --load "$f" --eval '(exit)');;
+      (semi) run=(semicolon "$f");;
+      (sf) run=(sidef "$f");;
+      (sim) compile=(cim "$f"); run=("./${f%.$ext}");;
+      (sl) run=(slsh "$f");;
+      (sml) compile=(mlton "$f"); run=("./${f%.$ext}");;
+      (sno) run=(snobol4 "$f");;
+      (spl) run=(splrun "$f");;
+      (sq) run=(subleq "$f");;
+      (st) run=(gst "$f");;
+      (tcl) run=(tclsh "$f");;
+      (ts) compile=(tsc "$f"); run=(node "${f%.$ext}.js");;
+      (unl) run=(unlambda "$f");;
+      (vala) compile=(valac "$f"); run=("./${f%.$ext}");;
+      (vq) run=(varaq -s "$f");;
+      (wren) run=(wren_cli "$f");;
+      (yorick) run=(yorick -batch "$f");;
+      (zig) run=(zig run "$f");;
+      (🍇) compile=(emojicodec "$f"); run=("./${f%.$ext}");;
+      (*) if type -t "$ext"  >/dev/null; then
            run=("$ext" "$f")
          else
            result='[33mSKIP[0m'
@@ -187,7 +190,7 @@ for lang in "${languages[@]}"; do
         fi
       fi
     fi
-    echo "$result"
+    echo "$result")
   fi
 done
 if (( keep )); then
